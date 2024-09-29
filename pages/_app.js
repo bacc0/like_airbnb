5269ce5a242c40705695705056b512a6313dff0e
Veselin Hr. Trayanov <1.1@gmx.com>
2024-09-29 22:02:34 +0100

    favicon

--- pages/_app.js
+++ pages/_app.js
@@ -18,7 +18,7 @@ export default function MyApp(props) {
   return (
     <CacheProvider value={emotionCache}>
       <Head>
-        <title>My page</title>
+        <title>ALIBMB</title>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
         <meta name="theme-color" content="#FF385C"></meta>
       </Head>
